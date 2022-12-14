dda6553ad3d256928b8a79bb809d38da1f077e11
Lucas Zanek <lucaszanek94@gmail.com>
2022-12-13 22:54:25 -0300

    [connection_manager]: move the read into a separated thread for non blocking code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::io::prelude::BufRead;
+use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
 
 fn main() {
+    // Bind to the local IP address and port 7878
     let listener = match TcpListener::bind("127.0.0.1:7878") {
         Ok(listener) => {
-            println!("Connection stablished");
+            println!("Connection established");
             listener
         }
         Err(e) => {
@@ -12,6 +17,7 @@ fn main() {
         }
     };
 
+    // Accept incoming connections
     for stream in listener.incoming() {
         let stream = match stream {
             Ok(stream) => stream,
@@ -20,8 +26,30 @@ fn main() {
                 return;
             }
         };
-        println!("Connection established!");
+        connection_manager(stream);
     }
 
     println!("Hello, world!");
+}
+
+fn connection_manager(mut stream: TcpStream) {
+    let mut buffer = Vec::new();
+
+    thread::spawn(move || match stream.read_to_end(&mut buffer) {
+        Ok(result) => {
+            println!("Read {} bytes from stream", result);
+            println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
+        }
+        Err(e) => {
+            println!("Error reading from stream: {}", e);
+        }
+    });
+
+    let response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
+
+    // Send the response back to the client
+    match stream.write(response.as_bytes()) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Error writing to stream: {}", e),
+    }
 }
