353235e63d1f73122b1d1b52f52e70d284f7553a
Lucas Zanek <lucaszanek94@gmail.com>
2022-12-13 22:04:42 -0300

    [TCP] Connection binding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::net::TcpListener;
 
 fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listener) => {
+            println!("Connection stablished");
+            listener
+        }
+        Err(e) => {
+            println!("Failed to bind to address with this error {}", e);
+            return;
+        }
+    };
+
     println!("Hello, world!");
 }
