170a2e70683315d32e43dc8a28fcd97acbb1a779
Lucas Zanek <lucaszanek94@gmail.com>
2022-12-14 10:31:25 -0300

    [connection_manager]: back to 1 thread and return file for simplify mockup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::prelude::BufRead;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::thread;
+use std::{fs, thread};
 
 fn main() {
     // Bind to the local IP address and port 7878
@@ -33,23 +33,23 @@ fn main() {
 }
 
 fn connection_manager(mut stream: TcpStream) {
-    let mut buffer = Vec::new();
+    // TODO: Modify to allow dinamyc sizes
+    // TODO: Manage the unrwap calls to handle the errors;
+    let mut buffer = [0; 1024];
 
-    thread::spawn(move || match stream.read_to_end(&mut buffer) {
-        Ok(result) => {
-            println!("Read {} bytes from stream", result);
-            println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            let contents = fs::read_to_string("index.html").unwrap();
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                contents.len(),
+                contents
+            );
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap()
         }
         Err(e) => {
-            println!("Error reading from stream: {}", e);
+            println!("Error:{}", e)
         }
-    });
-
-    let response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
-
-    // Send the response back to the client
-    match stream.write(response.as_bytes()) {
-        Ok(_) => println!("Response sent"),
-        Err(e) => println!("Error writing to stream: {}", e),
     }
 }
