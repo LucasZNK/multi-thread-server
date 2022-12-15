6248fb2efa060f116018e00d1663d540414460f5
Lucas Zanek <lucaszanek94@gmail.com>
2022-12-15 12:18:54 -0300

    [multi-threads]: library, creating multi thread spawns

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,10 @@
-use std::io::prelude::BufRead;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::time::Duration;
 use std::{fs, thread};
+
+use web_server::ThreadPool;
 
 fn main() {
     // Bind to the local IP address and port 7878
@@ -17,6 +19,8 @@ fn main() {
         }
     };
 
+    let pool = ThreadPool::new(4);
+
     // Accept incoming connections
     for stream in listener.incoming() {
         let stream = match stream {
@@ -26,10 +30,10 @@ fn main() {
                 return;
             }
         };
-        connection_manager(stream);
+        pool.execute(|| {
+            connection_manager(stream);
+        });
     }
-
-    println!("Hello, world!");
 }
 
 fn connection_manager(mut stream: TcpStream) {
@@ -51,7 +55,12 @@ fn connection_manager(mut stream: TcpStream) {
 }
 
 fn response_builder(buffer: &[u8], get: &[u8]) -> String {
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(!size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {
+            //create threads
+        }
+        ThreadPool { threads }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
