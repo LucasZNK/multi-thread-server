d9bd57fe83271606f8dac8264c353178c62cbef1
Lucas Zanek <lucaszanek94@gmail.com>
2022-12-27 14:37:54 -0300

    [thrad_pool:build]: added build function and docs comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+use std::error;
+use std::thread;
+
+/// An error that can occur when creating a `ThreadPool`.
+#[derive(Debug)]
+pub enum PoolCreationError {
+    /// The size of the thread pool must be greater than zero.
+    InvalidSize,
+}
+
+impl error::Error for PoolCreationError {}
+
+impl std::fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid thread pool size")
+    }
+}
+
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Creates a new `ThreadPool` with the specified number of worker threads.
+    /// # Panics
+    /// This function will panic if `size` is zero.
+
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -12,4 +34,21 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
     }
+}
+
+/// Builds a new `ThreadPool` with the specified size.
+/// # Errors
+/// This function will return an error if the size of the thread pool is zero.
+/// # Examples
+/// ```
+/// use threadpool::{ThreadPool, build};
+///
+/// let pool = build(4).unwrap();
+/// ```
+pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+    if size == 0 {
+        return Err(PoolCreationError::InvalidSize);
+    }
+
+    Ok(ThreadPool::new(size))
 }
